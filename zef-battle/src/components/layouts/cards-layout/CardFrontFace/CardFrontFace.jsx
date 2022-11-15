5a51a8be41433924b9496b89bfbfaa23cc6581e2
Pololops <paul.rigaudeau@me.com>
2022-11-15 10:16:59 +0100

    modifier button added to character cards

--- zef-battle/src/components/layouts/cards-layout/CardFrontFace/CardFrontFace.jsx
+++ zef-battle/src/components/layouts/cards-layout/CardFrontFace/CardFrontFace.jsx
@@ -5,16 +5,44 @@ import Capacity from '../Capacity/Capacity';
 export default function CardFrontFace({ title, capacities }) {
 	return (
 		<div className="card__inner__face card__inner__face--front">
-			<span className="card__inner__face__title">{title}</span>
-			{capacities.length > 0 &&
-				capacities.map(({ id, name, level, description }) => (
-					<Capacity
-						key={id + name}
-						name={name}
-						level={level}
-						desc={description}
-					/>
-				))}
+			<div className="card__inner__face__infos">
+				<span className="card__inner__face__title">{title}</span>
+
+				<button className="button button--submit">Modifier</button>
+
+				<form className="form" style={{ display: 'none' }}>
+					<button className="button button--submit">
+						Ajouter une capacité
+					</button>
+					<div className="form__buttons">
+						<button className="button button--submit">Modifier</button>
+						<button className="button button--reset">Supprimer</button>
+					</div>
+				</form>
+			</div>
+
+			<div className="capacities">
+				{capacities.length > 0 &&
+					capacities.map(({ id, name, level, description }) => (
+						<Capacity
+							key={id + name}
+							name={name}
+							level={level}
+							desc={description}
+						/>
+					))}
+
+				<div className="capacity">
+					<span className="capacity__label">name</span>
+					<div className="capacity__meter">
+						<div
+							className="capacity__meter__level"
+							style={{ width: `50%`, backgroundColor: 'red' }}
+						></div>
+					</div>
+					<div className="capacity__description">desc</div>
+				</div>
+			</div>
 		</div>
 	);
 }
