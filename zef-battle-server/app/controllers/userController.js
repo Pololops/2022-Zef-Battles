defdd16b9ebe364fdc9a1eba1c11abe09b5db4de
Paul Rigaudeau <paul.rigaudeau@me.com>
2023-01-06 18:32:05 +0100

    user password encryption implemented

--- zef-battle-server/app/controllers/userController.js
+++ zef-battle-server/app/controllers/userController.js
@@ -1,10 +1,18 @@
 import Debug from 'debug'
 const debug = Debug('Controller:userController:log')
 
+import {
+	compare as bcryptCompare,
+	genSalt as bcryptGenSalt,
+	hash as bcryptHash,
+} from 'bcrypt'
+
 import ApiError from '../errors/apiError.js'
 
 import userDatamapper from '../models/user.js'
 import { tokenGenerator } from '../middlewares/tokenManager.js'
+
+const bcryptSalt = await bcryptGenSalt(10)
 
 export default {
 	getAll: async (_request, response) => {
@@ -38,6 +46,12 @@ export default {
 			})
 		}
 
+		const encryptedPassword = await bcryptHash(
+			request.body.password,
+			bcryptSalt,
+		)
+		request.body.password = encryptedPassword
+
 		const savedUser = await userDatamapper.insert(request.body)
 
 		debug('create : ', savedUser)
@@ -60,9 +74,11 @@ export default {
 			}
 		}
 
+		const encryptedPassword = await bcryptHash(user.password, bcryptSalt)
+
 		const userToSave = {
 			name: request.body.name,
-			password: request.body.password
+			password: encryptedPassword,
 		}
 		if (request.connectedUser.role === 'admin') {
 			userToSave.role = request.body.role
@@ -86,7 +102,10 @@ export default {
 
 	login: async (request, response) => {
 		const foundUser = await userDatamapper.findByName(request.body.login)
-		if (!foundUser || foundUser.password !== request.body.password)
+		if (
+			!foundUser || 
+			!(await bcryptCompare(request.body.password, foundUser.password))
+		)
 			throw new ApiError('Login and password are not correct', {
 				statusCode: 400,
 			})
@@ -99,7 +118,7 @@ export default {
 				id: foundUser.id,
 				name: foundUser.name,
 				victory_number: foundUser.victory_number,
-				role: foundUser.role
+				role: foundUser.role,
 			},
 		})
 	},
